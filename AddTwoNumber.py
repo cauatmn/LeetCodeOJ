322c18d083750eae6371aa7037bde4569deacc7e
= <=>
2015-12-22 14:12:11 +0800

    change 1

--- AddTwoNumber.py
+++ AddTwoNumber.py
@@ -7,3 +7,38 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
+
+class ListNode(object):
+	def __init__(self,x):
+		self.val = x
+		self.next = None
+		
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy,flag = ListNode(0),0
+        head = dummy
+        while flag or l1 or l2:
+            node = ListNode(flag)
+            if l1:
+                node.val +=l1.val
+                l1 = l1.next
+            if l2:
+                node.val +=l2.val
+                l2 = l2.next
+            flag = node.val / 10 
+            node.val %= 10
+            head.next = node
+            head = head.next			
+        return dummy.next
+		
+if __name__ == '__main__':
+	L1 = [0]
+	L2 = [0]
+	Test = Solution()
+	output = Test.addTwoNumbers(L1,L2)
+	
